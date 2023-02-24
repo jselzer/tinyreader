ac63ca23f00ef63a91835f86f7d9a7d244feecae
Jason Selzer <selzerj@gmail.com>
2023-02-23 19:53:22 -0600

    Initial commit

---
+++ src/main/java/com/selzerj/tinyreader/TinyreaderApplication.java
@@ -0,0 +1,13 @@
+package com.selzerj.tinyreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyreaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyreaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/selzerj/tinyreader/TinyreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.selzerj.tinyreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinyreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
