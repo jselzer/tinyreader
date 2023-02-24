6b990018076da91c774a0d5c071d7850adb0842d
Jason Selzer <selzerj@gmail.com>
2023-02-23 21:17:13 -0600

    fix capitalization

--- src/main/java/com/selzerj/tinyreader/TinyReaderApplication.java
+++ src/main/java/com/selzerj/tinyreader/TinyReaderApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class TinyreaderApplication {
+public class TinyReaderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TinyreaderApplication.class, args);
+		SpringApplication.run(TinyReaderApplication.class, args);
 	}
 
 }
